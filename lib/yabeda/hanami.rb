6745184ea08fc47b32e99ccd9d03b340cefe566b
Greg Kostin <gkostin@umich.edu>
2024-03-13 14:10:24 -0400

    bundle gem yabeda-hanami

---
+++ lib/yabeda/hanami.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hanami/version"
+
+module Yabeda
+  module Hanami
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/yabeda/hanami_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Yabeda::Hanami do
+  it "has a version number" do
+    expect(Yabeda::Hanami::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
